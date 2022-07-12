d7fd72ca706319193db7b40e15a4dcb8f17bc27b
Mark Schewe <mark.schewe@sigint.io>
2022-07-12 13:42:31 +0200

    creates option 1

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
-<h1 class="text-red-500">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="container py-3 mx-auto">
+	<h1 class="py-4 text-2xl">Sveltekit Mobile Application</h1>
+
+	<button class="px-3 py-2 border" on:click={() => goto('/option1')}>Options 1</button>
+</div>
---
+++ src/routes/option1.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-col h-screen max-w-md mx-auto ">
+	<header class="bg-blue-100">Header</header>
+
+	<main class="flex-1 bg-red-100">
+		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error a autem cupiditate! Nulla optio
+		deserunt maiores voluptate nemo rerum deleniti quasi tempore incidunt, eum iusto fugit suscipit
+		amet! Corrupti, enim.
+	</main>
+
+	<footer class="bg-green-100">Footer</footer>
+</div>
