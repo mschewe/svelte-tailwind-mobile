7365f65bfc4ecc9724add899cf8ecb37c000b94c
Mark Schewe <mark.schewe@sigint.io>
2022-07-12 13:46:14 +0200

    adds overflow css

--- src/routes/option1.svelte
+++ src/routes/option1.svelte
@@ -1,7 +1,7 @@
-<div class="flex flex-col h-screen max-w-md mx-auto ">
+<div class="flex flex-col h-screen max-w-md mx-auto overflow-hidden">
 	<header class="bg-blue-100">Header</header>
 
-	<main class="flex-1 bg-red-100">
+	<main class="flex-1 overflow-y-auto bg-red-100">
 		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error a autem cupiditate! Nulla optio
 		deserunt maiores voluptate nemo rerum deleniti quasi tempore incidunt, eum iusto fugit suscipit
 		amet! Corrupti, enim.
