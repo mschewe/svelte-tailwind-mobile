0a231989cecd9df6c9132dd6df23fa3162afc7cc
Mark Schewe <mark.schewe@sigint.io>
2022-07-12 13:48:13 +0200

    adds a little bit of styling

--- src/routes/option1.svelte
+++ src/routes/option1.svelte
@@ -1,11 +1,11 @@
 <div class="flex flex-col h-screen max-w-md mx-auto overflow-hidden">
-	<header class="bg-blue-100">Header</header>
+	<header class="flex items-center justify-center h-10 bg-blue-100">Header</header>
 
-	<main class="flex-1 overflow-y-auto bg-red-100">
+	<main class="flex-1 p-2 overflow-y-auto bg-red-100">
 		Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error a autem cupiditate! Nulla optio
 		deserunt maiores voluptate nemo rerum deleniti quasi tempore incidunt, eum iusto fugit suscipit
 		amet! Corrupti, enim.
 	</main>
 
-	<footer class="bg-green-100">Footer</footer>
+	<footer class="flex items-center justify-center h-10 bg-green-100">Footer</footer>
 </div>
